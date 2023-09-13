cd59523205898e7672c57a9154d48fb9961cd1f8
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2023-09-13 13:23:46 +0200

    extend dependencies

--- packages/components/bumpstack.sh
+++ packages/components/bumpstack.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+!/bin/bash
 
-# npm un -S @papillonarts/css @papillonarts/library && npm i -S @papillonarts/css @papillonarts/library
+npm un -S @papillonarts/css @papillonarts/library && npm i -S @papillonarts/css @papillonarts/library
 
-# npm run clean-up && npm run install-packages && npm audit fix
+npm run clean-up && npm run install-packages && npm audit fix
