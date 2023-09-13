5d6a772048f50daa41cfb56c7eae41b89367281f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2023-09-13 11:41:45 +0200

    setup prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// const prettierSetup = require('@papillonarts/setup/prettier')
+
+// module.exports = { ...prettierSetup }
+
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 140,
+  trailingComma: 'all',
+}
