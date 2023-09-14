068afcdb382debddc11f87391f1a5260731af7c3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2023-09-14 13:36:21 +0200

    implement a11y js library

--- packages/library/src/a11y/index.js
+++ packages/library/src/a11y/index.js
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 // https://github.com/dequelabs/axe-core-npm
 // https://www.npmjs.com/package/@axe-core/react
 
 /* eslint-disable global-require */
 
-export function checkAccessibilityIssues(react: unknown, reactDOM: unknown, delay: number) {
+export function checkAccessibilityIssues(react, reactDOM, delay) {
   if (process.env.NODE_ENV !== 'production') {
-    import axe = require('@axe-core/react')
-
+    const axe = require('@axe-core/react')
     axe(react, reactDOM, delay)
   }
 }
